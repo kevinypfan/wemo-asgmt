ca19a346e4928d10c1774e0efb07014bc863a7d7
KevinFan <kevinypfan@gmail.com>
2023-03-11 20:17:40 +0800

    db config done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.${NODE_ENV}.local`, `.env.${NODE_ENV}`],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PG_HOST,
+      port: parseInt(process.env.PG_PORT || '5432'),
+      username: process.env.PG_USERNAME,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE,
+      synchronize: true,
+      logging: true,
+      autoLoadEntities: true,
+      subscribers: [],
+      migrations: [],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
