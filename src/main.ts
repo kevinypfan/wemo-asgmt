014073b860a67aec3faaa476ed8fcc8cae7733b2
KevinFan <kevinypfan@gmail.com>
2023-03-15 15:15:41 +0800

    swagger docker setup deployed endpoint url

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,16 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
+  const configBuilder = new DocumentBuilder()
     .setTitle('Wemo assignment')
     .setDescription('Wemo rent scooter backend assignment')
     .setVersion('0.0.1')
-    .addBearerAuth()
-    .build();
+    .addBearerAuth();
+
+  if (process.env.NODE_ENV === 'production')
+    configBuilder.addServer(process.env.ENDPOINT_URL);
+
+  const config = configBuilder.build();
 
   app.useGlobalPipes(new ValidationPipe());
   const document = SwaggerModule.createDocument(app, config);
